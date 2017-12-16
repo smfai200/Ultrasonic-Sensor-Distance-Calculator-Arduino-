b4ccf1aef80b8024506200213411f1c0d5361b1b
Syed Faizan <smfai200@gmail.com>
2017-12-17 00:08:00 +0500

    Add files via upload

---
+++ sketch_dec16b.ino
@@ -0,0 +1,43 @@
+const int trigPin = 12;
+const int echoPin = 11;
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  long duration, inches, cm;
+//send the sonic
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+//calculate length of the pulse
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(10000);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
